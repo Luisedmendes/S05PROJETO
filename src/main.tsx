72b2f3905b193f2c1e4f09d4b577d7909608ccb1
luis <luis.e@ges.inatel.br>
2025-06-08 20:30:21 -0300

    feat: project

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/S05PROJETO">
       <App />
     </BrowserRouter>
   </React.StrictMode>
